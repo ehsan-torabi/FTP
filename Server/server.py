20824e4d120f65db9bd423b88db012f1d78b2954
ehsan <torabi782@gmail.com>
2024-11-29 14:32:28 +0330

    - Add SERVER_PUBLIC_PATH global constant - Refactor some sections

--- Server/server.py
+++ Server/server.py
@@ -1,49 +1,50 @@
-import asyncio
 import os
-import threading
 import socket
-import sys
-sys.path.append(os.path.curdir)
+import threading
 from Server.util import command_manage
 
+SERVER_PUBLIC_PATH = os.path.abspath("../Public")
 
-class server:
+class Server:
     def __init__(self, port) -> None:
         self.port = port
 
-    def startServer(self):
+    def start_server(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         while True:
             try:
-                self.port += 1
                 sock.bind(("", self.port))
                 break
             except OSError:
-                continue
-        
-        sock.listen(0)
-        print(f"Server run on {self.port}")
+                self.port += 1
+
+        sock.listen(5)  # Allow up to 5 connections
+        print(f"Server running on port {self.port}")
         return sock
 
 
-def readConnection(conn:socket.socket,addr):
+def read_connection(conn: socket.socket, addr):
+    conn.send("[+] You are connected.\nUse help to see commands.\n".encode())
+    print(f"[+] User connected: {addr}")
     while True:
-        data = conn.recv(2048)
-        if data.decode().strip().lower() == "quit":
-            conn.close()
+        data = conn.recv(1024)
+        if not data:
             break
-        command_manage.command_parser(data.decode(),conn,addr)
+        command_manage.command_parser(data.decode(), conn, addr)
+
 
 def main():
-    with server(8021).startServer() as srv:
+    with Server(8021).start_server() as srv:
         while True:
             try:
                 conn, addr = srv.accept()
-            except:
+            except Exception as e:
+                print(f"Error accepting connection: {e}")
                 continue
-            threading.Thread(target=readConnection,args=(conn,addr),daemon=False).start() 
-            
+            t = threading.Thread(target=read_connection, args=(conn, addr))
+            t.start()
+
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
