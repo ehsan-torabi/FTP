d371db8a2493bebc82a9bcfe46f135a006251486
ehsan <torabi782@gmail.com>
2024-11-29 14:37:42 +0330

    - Remove locks - Add some command handling - Refactor some section

--- Client/client.py
+++ Client/client.py
@@ -1,28 +1,31 @@
 import socket as s
 import threading as thr
+from utils import send_file
 
-op_lock = thr.Lock()
-
-def getData(soc): 
+def get_data(usr_socket):
     while True:
-        data = soc.recv(1024)
-        if data:
-            print(data.decode(),end="")
+        data = usr_socket.recv(1024)
+        if not data:
+            break
+        print(data.decode(), end="")
 
-
-def sendData(soc):
+def send_data(usr_socket):
     while True:
-        op_lock.acquire()
-        soc.sendall(input().encode())
-        op_lock.release()
+        user_input = input()
+        args = user_input.split(" ")[1:]
+        usr_socket.sendall(user_input.encode())
+        if user_input.lower().startswith("upload"):
+            send_file.upload_file(usr_socket, args[0])
+        if user_input.lower().startswith("quit"):
+            usr_socket.shutdown(0)
+            return
 
 port = input("Enter Port: ")
-with s.socket(s.AF_INET,s.SOCK_STREAM) as soc:
-    soc.connect(('',int(port)))
-    t1 = thr.Thread(target=getData,args=(soc,))
-    t2 = thr.Thread(target=sendData,args=(soc,))
+with s.socket(s.AF_INET, s.SOCK_STREAM) as soc:
+    soc.connect(('', int(port)))
+    t1 = thr.Thread(target=get_data, args=(soc,))
+    t2 = thr.Thread(target=send_data, args=(soc,))
     t1.start()
     t2.start()
     t1.join()
-    t2.join
-        
+    t2.join()
