6053d5a1cae78de76084fd75ee83485287d42d27
ehsan <torabi782@gmail.com>
2024-11-29 11:20:56 +0330

    - Change static address to dynamic

--- Server/util/help_handler.py
+++ Server/util/help_handler.py
@@ -1,7 +1,9 @@
 import socket
+import os
 
 
-def show_help(conn:socket.socket):
-    with open("Server/util/assets/help.md","r") as f:
-        help_contetnt = f.read()
-        conn.sendall(help_contetnt.encode("utf-8"))
+def show_help(conn: socket.socket):
+    os.getcwd()
+    with open(os.path.join(os.getcwd(),"util", "assets",  "help.txt"), "r") as f:
+        help_content = f.read()
+        conn.sendall(help_content.encode("utf-8"))
