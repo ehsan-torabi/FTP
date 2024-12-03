bce68db3c15465906f41badd30749757bcd65941
ehsan <torabi782@gmail.com>
2024-12-03 23:49:44 +0330

    - Remove user_accept filed and command_accept - Add accept filed

--- Server/util/standard_response.py
+++ Server/util/standard_response.py
@@ -2,14 +2,11 @@ import json
 
 
 class StandardResponse:
-    def __init__(self,user_accept:bool,command_accept:bool,status_code:int,data:any=None):
-        self.user_accept = user_accept
-        self.command_accept = command_accept
+    def __init__(self, accept: bool, status_code: int, data: any = None):
+        self.accept = accept
         self.status_code = status_code
         self.data = data
 
     def serialize(self):
-        query_data = {"user_accept":self.user_accept,"command_accept":self.command_accept,"status_code":self.status_code,"data":self.data}
-        return json.dumps(query_data,ensure_ascii=False)
-
-
+        query_data = {"accept": self.accept, "status_code": self.status_code, "data": self.data}
+        return json.dumps(query_data, ensure_ascii=False)
