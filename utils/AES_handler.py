88ea58e8bf49df91a1aada3970a069535b096d22
ehsan <torabi782@gmail.com>
2024-12-05 17:16:28 +0330

    - Cleanup and refactor

--- utils/AES_handler.py
+++ utils/AES_handler.py
@@ -1,8 +1,8 @@
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
 import marshal
 
-from Crypto.Util.Padding import pad,unpad
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
 
 
 class AES_encryptor(object):
@@ -19,37 +19,34 @@ class AES_encryptor(object):
         with open(".aes_key", "wb") as key_file:
             key_file.write(marshal.dumps(information))
 
-    def encrypt_data(self, data: str | bytes)->(bytes,bytes):
+    def encrypt_data(self, data: str | bytes) -> (bytes, bytes):
         if isinstance(data, str):
             ciphertext = self.cipher.encrypt(pad(data.encode(), AES.block_size))
             old_iv = self.cipher.iv
             self.cipher = AES.new(self.key, AES.MODE_CBC)
-            return ciphertext,old_iv
+            return ciphertext, old_iv
         elif isinstance(data, bytes):
             ciphertext = self.cipher.encrypt(pad(data, AES.block_size))
             old_iv = self.cipher.iv
             self.cipher = AES.new(self.key, AES.MODE_CBC)
 
-            return ciphertext,old_iv
-
+            return ciphertext, old_iv
 
 
 class AES_decryptor(object):
-    def __init__(self, key:bytes):
+    def __init__(self, key: bytes):
         self.key = key
 
     @staticmethod
-    def read_key(key_file_path:str):
+    def read_key(key_file_path: str):
         with open(key_file_path, "rb") as key_file:
             information = marshal.loads(key_file.read())
             return AES_decryptor(information["key"])
 
-    def get_key(self)->bytes:
+    def get_key(self) -> bytes:
         return self.key
 
-    def decrypt_data(self,ciphertext,iv)-> bytes:
-        cipher = AES.new(self.key, AES.MODE_CBC,iv)
+    def decrypt_data(self, ciphertext, iv) -> bytes:
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
         decrypted = cipher.decrypt(ciphertext)
-        return unpad(decrypted,AES.block_size)
-
-
+        return unpad(decrypted, AES.block_size)
--- utils/request_parser.py
+++ utils/request_parser.py
@@ -2,10 +2,10 @@ import bz2
 import json
 
 
-def server_response_parser(response):
+def response_parser(response):
     decompressed = bz2.decompress(response)
     return json.loads(decompressed)
 
 
-def client_response_parser(response):
+def request_parser(response):
     return json.loads(response.decode('utf-8'))
