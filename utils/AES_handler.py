5256a0efa2c008d089e6a5b83b6b5c4c78fa5304
ehsan <torabi782@gmail.com>
2024-12-06 12:14:04 +0330

    - Fix iv problem and change to CFB

--- utils/AES_handler.py
+++ utils/AES_handler.py
@@ -2,51 +2,53 @@ import marshal
 
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import pad, unpad
 
 
 class AES_encryptor(object):
     def __init__(self, key_length: int):
         self.key_length = key_length
         self.key = get_random_bytes(key_length)
-        self.cipher = AES.new(self.key, AES.MODE_CBC)
+        self.iv = get_random_bytes(AES.block_size)
+        self.cipher = AES.new(self.key, AES.MODE_CFB,self.iv)
+
 
     def get_key(self) -> bytes:
         return self.key
-
+    def get_iv(self) -> bytes:
+        return self.iv
     def save_key(self):
-        information = {"key_length": self.key_length, "key": self.key}
+        information = {"key_length": self.key_length, "key": self.key, "iv": self.iv}
         with open(".aes_key", "wb") as key_file:
             key_file.write(marshal.dumps(information))
 
     def encrypt_data(self, data: str | bytes) -> (bytes, bytes):
         if isinstance(data, str):
-            ciphertext = self.cipher.encrypt(pad(data.encode(), AES.block_size))
+            ciphertext = self.cipher.encrypt(data.encode())
             old_iv = self.cipher.iv
-            self.cipher = AES.new(self.key, AES.MODE_CBC)
+            self.cipher = AES.new(self.key, AES.MODE_CFB,self.iv)
             return ciphertext, old_iv
         elif isinstance(data, bytes):
-            ciphertext = self.cipher.encrypt(pad(data, AES.block_size))
+            ciphertext = self.cipher.encrypt(data)
             old_iv = self.cipher.iv
-            self.cipher = AES.new(self.key, AES.MODE_CBC)
-
+            self.cipher = AES.new(self.key, AES.MODE_CFB,self.iv)
             return ciphertext, old_iv
 
 
 class AES_decryptor(object):
-    def __init__(self, key: bytes):
+    def __init__(self, key: bytes,iv: bytes):
         self.key = key
+        self.iv = iv
 
     @staticmethod
     def read_key(key_file_path: str):
         with open(key_file_path, "rb") as key_file:
             information = marshal.loads(key_file.read())
-            return AES_decryptor(information["key"])
+            return AES_decryptor(information["key"], information["iv"])
 
     def get_key(self) -> bytes:
         return self.key
 
-    def decrypt_data(self, ciphertext, iv) -> bytes:
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+    def decrypt_data(self, ciphertext) -> bytes:
+        cipher = AES.new(self.key, AES.MODE_CFB,self.iv)
         decrypted = cipher.decrypt(ciphertext)
-        return unpad(decrypted, AES.block_size)
+        return decrypted
